4dcda6372a4a6e0934dd658fdf6c479c3ec67874
AHMEDNABIB <nabibpallab@gmail.com>
2021-11-02 20:58:36 +0600

    Alert

--- src/App.js
+++ src/App.js
@@ -7,16 +7,40 @@ function App() {
   const [list, setList] = useState([])
   const [isEditing, setIsEditing] = useState(false)
   const[editId, setEditId] = useState(null)
-  const [alert, setAlert] =useState ({ show: false, msg: '', type: ''})
+  const [alert, setAlert] =useState ({ show: false , msg: '', type: ''})
  
   const handleSubmit=(e) =>{
     e.preventDefault()
-    console.log(' hello')
+    if (!name) {
+      showAlert(true, 'danger', 'please enter value')
+    } else if(name && isEditing){
+      
+    }else {
+      showAlert(true, 'success', 'item added to the list')
+      const newItem = { id: new Date().getTime().toString(), title: name };
+
+      setList([...list, newItem]);
+      setName('');
+    }
+  }
+
+  const showAlert = (show = false, type= '', msg='') =>{
+    setAlert({show, type, msg})
+  }
+
+  const clearList = () =>{
+    showAlert(true, 'danger', 'empty  list')
+    setList([])
+  }
+
+  const removeItem =(id) =>{
+    showAlert(true, 'danger', 'Item remove from list')
+    setList(list.filter((item)=> item.id !==id ));
   }
   return (
     <section className='section-center'>
       <form className='grocery-form' onSubmit={handleSubmit}>
-      {alert.show && <Alert/>}
+      {alert.show && <Alert {...alert} removeAlert={showAlert} list={list}/>}
       <h3>grocerry bud</h3>
       <div className="form-control">
         <input 
@@ -29,10 +53,14 @@ function App() {
         <button type='submit' className='submit-btn'>{isEditing ? 'edit': 'submit'}</button>
       </div>
       </form>
+
+       {list.length> 0 && (
         <div className="grocery-container">
-          <List/>
-          <button className='clear-btn'>clear items</button>
+          <List items={list} removeItem={removeItem}/>
+          <button className='clear-btn' onClick={clearList} >clear items</button>
         </div>
+       )}
+       
     
     </section>
   )
